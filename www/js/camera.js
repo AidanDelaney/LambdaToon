dacd56da3e9f30480987e43ac41db734283f8516
Aidan Delaney <aidan@ontologyengineering.org>
2014-05-28 09:30:28 +0100

    Initial commit

---
+++ www/js/camera.js
@@ -0,0 +1,45 @@
+// Taken from the Cordova API examples
+function cameraApp(){}
+
+cameraApp.prototype={
+    _pictureSource: null,
+
+    _destinationType: null,
+
+    run: function(){
+        var that=this;
+        that._pictureSource = navigator.camera.PictureSourceType;
+        that._destinationType = navigator.camera.DestinationType;
+        id("capturePhotoButton").addEventListener("click", function(){
+            that._capturePhoto.apply(that,arguments);
+        });
+    },
+
+    _capturePhoto: function() {
+        var that = this;
+
+        // Take picture using device camera and retrieve image as base64-encoded string.
+        navigator.camera.getPicture(function(){
+            that._onPhotoDataSuccess.apply(that,arguments);
+        },function(){
+            that._onFail.apply(that,arguments);
+        },{
+            quality: 50,
+            destinationType: that._destinationType.DATA_URL
+        });
+    },
+
+    _onPhotoDataSuccess: function(imageData) {
+        var results = id('results');
+        var smallImage = document.createElement("img");
+        smallImage.style.display = 'block';
+
+        // Show the captured photo.
+        smallImage.src = "data:image/jpeg;base64," + imageData;
+        results.appendChild(smallImage);
+    },
+
+    _onFail: function(message) {
+        alert(message);
+    }
+}
