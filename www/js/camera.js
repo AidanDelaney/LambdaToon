1d444791e6153bf771de3e3c0ee4a7e0b094aff4
Aidan Delaney <aidan@ontologyengineering.org>
2014-05-29 00:50:51 +0100

    Added in a gallery into which we drop pictures.

--- www/js/camera.js
+++ www/js/camera.js
@@ -6,6 +6,10 @@ cameraApp.prototype={
 
     _destinationType: null,
 
+    gallery: function(gallery){
+        this._gallery = gallery;
+    },
+
     run: function(){
         var that=this;
         that._pictureSource = navigator.camera.PictureSourceType;
@@ -30,15 +34,23 @@ cameraApp.prototype={
     },
 
     _onPhotoDataSuccess: function(imageData) {
-        var results = id('results');
-        var smallImage = document.createElement("img");
-        smallImage.style.display = 'block';
-        smallImage.style.width  = '120px';
-        smallImage.style.height = '120px';
+        var li  = document.createElement("li");
+        var a   = document.createElement("a");
+        var img = document.createElement("img");
+        var uuid = generateUUID();
+
+        //a.href = '#' + uuid;
+        // complete and total hack
+        a.href = "data:image/jpeg;base64," + imageData;
+        a.classList.add('thumb');
 
         // Show the captured photo.
-        smallImage.src = "data:image/jpeg;base64," + imageData;
-        results.appendChild(smallImage);
+        img.id  = uuid;
+        img.src = "data:image/jpeg;base64," + imageData;
+
+        a.appendChild(img);
+        li.appendChild(a);
+        this._gallery.insertImage(li.outerHTML, this._gallery.data.length);
     },
 
     _onFail: function(message) {
